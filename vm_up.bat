b93c5728cb1325abbac187ec3448919114e15320
Nguyen Truong Anh Minh <minhnta@vietnamlab.vn>
2024-10-09 11:24:15 +0700

    Add Python automation script to create Ansible settings
    Add inventory.ini.example

--- vm_up.bat
+++ vm_up.bat
@@ -12,3 +12,34 @@ cd Vagrant/UbuntuServer
 vagrant up
 echo "Opening SSH Connection to UbuntuServer VM"
 start cmd /k "vagrant ssh"
+
+echo "Using Python to create environment for Ansible"
+
+:: Check if Python is installed
+where python >nul 2>nul
+if %errorlevel% == 0 (
+    echo Python is installed.
+    python --version
+) else (
+    echo Python is not installed.
+    echo Please setup Ansible manually
+    goto :exit
+)
+
+if not defined PYTHON (set PYTHON=python)
+if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
+
+for /f "delims=" %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME="%%i"
+
+if not exist "%VENV_DIR%" (
+    echo Creating virtual environment...
+    %PYTHON_FULLNAME% -m venv %VENV_DIR%
+) else (
+    echo Virtual environment already exists.
+)
+
+set PYTHON="%VENV_DIR%\Scripts\Python.exe"
+%PYTHON% -m pip install -r requirements.txt
+%PYTHON% init.py
+
+:exit
